3c499b6031f5a94b41646c4c9a93febad8f7ae2c
YikunLi <675217399@qq.com>
2017-05-19 18:31:49 +0800

    Add files via upload

--- DongNao_homework_UI_7/app/src/main/java/com/dongnao/homework7/MainActivity.java
+++ DongNao_homework_UI_7/app/src/main/java/com/dongnao/homework7/MainActivity.java
@@ -1,13 +1,60 @@
 package com.dongnao.homework7;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout mLinearLayout;
+
+    private RevealHolder[] mDrawables = { new RevealHolder(R.drawable.avft_active, R.drawable.avft),
+            new RevealHolder(R.drawable.box_stack_active, R.drawable.box_stack),
+            new RevealHolder(R.drawable.bubble_frame_active, R.drawable.bubble_frame),
+            new RevealHolder(R.drawable.bubbles_active, R.drawable.bubbles),
+            new RevealHolder(R.drawable.bullseye_active, R.drawable.bullseye),
+            new RevealHolder(R.drawable.circle_filled_active, R.drawable.circle_filled),
+            new RevealHolder(R.drawable.circle_outline_active, R.drawable.circle_outline) };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        this.setContentView(R.layout.activity_main);
+        this.mLinearLayout = (LinearLayout) this.findViewById(R.id.linearLayout);
+        for (int i = 0; i < this.mDrawables.length; i++) {
+            RevealView view = new RevealView(this);
+            view.setDrawable(this.mDrawables[i].activeId, this.mDrawables[i].inActiveId);
+            view.setRevealPercent((float) (1.0 / (i + 1)));
+            this.mLinearLayout.addView(view);
+        }
+        // TODO
+        // this.findViewById(R.id.scrollView).setOnScrollChangeListener(new
+        // View.OnScrollChangeListener() {
+        // @Override
+        // public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX,
+        // int oldScrollY) {
+        // for (int i = 0; i < mLinearLayout.getChildCount(); i++) {
+        // View child = mLinearLayout.getChildAt(i);
+        // float start = mLinearLayout.getLeft()
+        // + (mLinearLayout.getWidth() - child.getWidth()) / 2;
+        // float end = start + child.getWidth();
+        // if (child.getX() >= start && child.getX() <= end) {
+        // ((RevealView) child).setRevealPercent(0);
+        // } else {
+        // ((RevealView) child).setRevealPercent(1);
+        // }
+        // }
+        // }
+        // });
+    }
+
+    static class RevealHolder {
+        int activeId;
+        int inActiveId;
+
+        RevealHolder(int activeId, int inActiveId) {
+            this.activeId = activeId;
+            this.inActiveId = inActiveId;
+        }
     }
 }
--- DongNao_homework_UI_7/app/src/main/java/com/dongnao/homework7/RevealView.java
+++ DongNao_homework_UI_7/app/src/main/java/com/dongnao/homework7/RevealView.java
@@ -1,6 +1,10 @@
 package com.dongnao.homework7;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -11,19 +15,88 @@ import android.view.View;
 
 public class RevealView extends View {
 
+    private static final String TAG = "RevealView";
+
+    private Drawable mActiveDrawable;
+
+    private Drawable mInActiveDrawable;
+
+    private float mRevealPercent;
+
     public RevealView(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public RevealView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public RevealView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+        this(context, attrs, defStyleAttr, 0);
     }
 
-    public RevealView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+    public RevealView(Context context, @Nullable AttributeSet attrs, int defStyleAttr,
+            int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+
+        this.setRevealPercent(0.6f);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if (this.mActiveDrawable == null || this.mInActiveDrawable == null) {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+            return;
+        }
+        int maxWidth = this.mActiveDrawable.getIntrinsicWidth();
+        int maxHeight = this.mActiveDrawable.getIntrinsicHeight();
+        this.mActiveDrawable.setBounds(0, 0, this.mActiveDrawable.getIntrinsicWidth(),
+                this.mActiveDrawable.getIntrinsicHeight());
+        this.mInActiveDrawable.setBounds(0, 0, this.mInActiveDrawable.getIntrinsicWidth(),
+                this.mInActiveDrawable.getIntrinsicHeight());
+        maxWidth = maxWidth > this.mInActiveDrawable.getIntrinsicWidth() ? maxWidth
+                : this.mInActiveDrawable.getIntrinsicWidth();
+        maxHeight = maxHeight > this.mInActiveDrawable.getIntrinsicHeight() ? maxHeight
+                : this.mInActiveDrawable.getIntrinsicHeight();
+        this.setMeasuredDimension(maxWidth, maxHeight);
+    }
+
+    public void setDrawable(@DrawableRes int activeId, @DrawableRes int inActiveId) {
+        this.mActiveDrawable = this.getContext().getDrawable(activeId);
+        this.mInActiveDrawable = this.getContext().getDrawable(inActiveId);
+        this.requestLayout();
+    }
+
+    /**
+     * @param percent
+     */
+    public void setRevealPercent(float percent) {
+        this.mRevealPercent = percent;
+        this.invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (this.mActiveDrawable == null || this.mInActiveDrawable == null) {
+            return;
+        }
+
+        int saveCount = canvas.save();
+        RectF activeRectF = new RectF(0, 0,
+                this.mActiveDrawable.getIntrinsicWidth() * (1 - this.mRevealPercent),
+                this.mActiveDrawable.getIntrinsicHeight());
+        canvas.clipRect(activeRectF);
+        this.mActiveDrawable.draw(canvas);
+        canvas.restoreToCount(saveCount);
+
+        saveCount = canvas.save();
+        RectF inActiveRegion = new RectF(
+                this.mInActiveDrawable.getIntrinsicWidth() * (1 - this.mRevealPercent), 0,
+                this.mInActiveDrawable.getIntrinsicWidth(),
+                this.mInActiveDrawable.getIntrinsicHeight());
+        canvas.clipRect(inActiveRegion);
+        this.mInActiveDrawable.draw(canvas);
+        canvas.restoreToCount(saveCount);
     }
 }
